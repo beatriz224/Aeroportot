212fdad8dc1dd4b6137b166b968a314f7e94a017
beatriz224 <beatriz.pedroso.silva2009@gmail.com>
2026-04-23 08:33:07 -0300

    Add files via upload

--- models/Aeronave .js
+++ models/Aeronave .js
@@ -8,4 +8,19 @@ class Aeronave {
         this.destino = destino;
 
     }
-}
+      get lercombustivel() {
+        return `O tanque do voo ${this.#codigo} está em ${this.#combustivel}%`;
+    }
+
+    // 3. Crie o SETTER (A porta segura que permite ALTERAR, mas com regras!)
+    set abastecer(quantidade) {
+        if (quantidade < 0) {
+            console.log("Erro de segurança: Não é possível tirar combustível!");
+        } else if (this.#combustivel + quantidade > 100) {
+            console.log("Erro: O tanque vai transbordar! Limite é 100%.");
+        } else {
+            this.#combustivel += quantidade;
+            console.log(`Abastecimento concluído. Novo nível: ${this.#combustivel}%`);
+        }
+    } 
+}import {Voo} from "./voo.js/";
